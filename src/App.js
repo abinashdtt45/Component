7b973b175fbb6fcdb744dfe7f08ea4cc3753d5b6
Abinash Dutta <abinashdtt45@gmail.com>
2023-02-14 16:52:56 +0530

    Buttons with tailwind, classnames, react-icons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,45 @@
+import Button from "./components/Button";
+import { GoBell, GoArchive, GoBriefcase } from "react-icons/go";
+
 const App = () => {
-  return <div>App</div>;
+  const handleClick = () => {
+    console.log("clicked");
+  };
+  return (
+    <div>
+      <div>
+        <Button success rounded outline onClick={handleClick}>
+          <GoArchive />
+          Click Me
+        </Button>
+      </div>
+      <div>
+        <Button danger className="mb-5">
+          Buy Now
+        </Button>
+      </div>
+      <div>
+        <Button warning rounded>
+          <GoBell />
+          Click Offer
+        </Button>
+      </div>
+      <div>
+        <Button primary>Apply Offer</Button>
+      </div>
+      <div>
+        <Button danger outline>
+          <GoBriefcase />
+          Add to cart
+        </Button>
+      </div>
+      <div>
+        <Button secondary rounded>
+          Danger
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default App;
