33004a4db70ea180ff284ebd1a48de8e219fd335
Abinash Dutta <abinashdtt45@gmail.com>
2023-02-20 17:13:43 +0530

    Dropdown menu, Creating a common style Panel, useRef

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 // import Accordion from "./components/Accordion";
 // import ButtonPage from "./pages/ButtonPage";
 
-import AccordionPage from "./pages/AccordionPage";
+import DropdownPage from "./pages/DropdownPage";
+
+//import AccordionPage from "./pages/AccordionPage";
 
 const App = () => {
-  return <AccordionPage />;
+  return <DropdownPage />;
 };
 
 export default App;
